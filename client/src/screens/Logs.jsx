761c1f4906eef7522dc0e717562d68097684fd9a
April Shenk <aprilshenk@MacBook-Pro.local>
2020-10-21 09:11:47 -0400

    Updated controller for log index and have poses showing in log

--- client/src/screens/Logs.jsx
+++ client/src/screens/Logs.jsx
@@ -1,13 +1,45 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+  background: #F0F7F4;
+  margin: 0 auto;
+  padding: 10px;
+  width: 70%;
+  border-radius: 20px;
+`;
+
+const ButtonDiv = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  button {
+    background: #70ABAF;
+    width: 25%;
+    padding: 5px;
+    border-radius: 5px;
+    border: none;
+  }
+  button:hover {
+    background: #32292F;
+    color: #F0F7F4;
+  }
+
+`;
 
 const Logs = ({ logs }) => {
 
   const logsJSX = 
     logs.map(log => (
-      <div key={log.id}>
+      <StyledDiv key={log.id}>
         <h6>{new Date(log.created_at).toLocaleString()}</h6>
-        <p>{log.description}</p>
-      </div>
+        <p>Poses: {log.poses[0].name}</p>
+        <p>Notes: {log.description}</p>
+        <ButtonDiv>
+          <button>Edit</button>
+          <button>Delete</button>
+        </ButtonDiv>
+        
+      </StyledDiv>
     ))
   
 
