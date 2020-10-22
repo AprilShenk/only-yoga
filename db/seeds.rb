e8990aa6dc4ad4356f9fa7ba666c4b8ace8e1514
April Shenk <aprilshenk@MacBook-Pro.local>
2020-10-22 12:21:08 -0400

    adding data

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,21 @@ puts "#{User.count} users created"
 @example_log = Log.create!(description: 'Balance was off today', user: @user)
 puts "#{Log.count} logs created"
 
-@downward_dog = Pose.create!(name: "Downward Dog", description: "Hands and feet on ground and hips high", difficulty: "Beginner", image: "https://www.yogajournal.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_472/MTQ2MTgwNzUyNzM5NTQyNDYx/feature-image-4252.webp")
+@downward_dog = Pose.create!(name: "Downward Dog", description: "On your hands and knees bring your hands slightly in front of shoulders. Turn your toes under and push your knees away from the floor bring your hips high and straightening your back.", difficulty: "Beginner", image: "https://www.yogajournal.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_620/MTQ3MTU2NzMwOTA2NjgyODMw/2hp_290_1721_bjk.webp")
+
+@bridge_pose = Pose.create!(name: "Bridge", description: "With knees bent lift hips keeping shoulders on ground. Move sholders together and lace fingers under.", difficulty: "Beginner", image: "https://www.yogajournal.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_860/MTQ3MTc0Njc5MDM4MTQxOTAy/2hp_286_0621_bjk.webp")
+
+@forward_fold = Pose.create!(name: "Forward Fold", description: "Standing with feet over hips, bend at the hips bringing your head towards the ground", difficulty: "Beginner", image: "https://www.yogajournal.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_620/MTQ2MTgwNzIwNzk5NTI0Mzg0/hpforwardbend2.webp")
+
+@childs_pose = Pose.create!(name: "Child's Pose", description: "Kneel with your kness slightly wider than your hips and big toes touching. Reach hands towards the front of the mat and lengthen your spine.", difficulty: "Beginner", image: "https://www.yogajournal.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_620/MTQ3MTU3MzE1MDIyMjM1NDM5/3yp_287_6604_gn_bjk.webp")
+
+@cobra = Pose.create!(name: "Cobra", description: "Lay face down and place hands under shoulders. With your elbows tucked in lift up the chest.", difficulty: "Beginner", image: "https://www.yogajournal.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_620/MTQ3MTU3MjczOTUxNjEwMzE4/3yp_287_6710_gn_bjk.webp")
+
+@crow_pose = Pose.create!(name: "Crow Pose", description: "Starting in a squat with knees wide and feet together. Tilt torso forward and lift hips allowing knees to rest on tricpes.Bring feet off the groud.", difficulty: "Intermediate", image: "https://www.yogajournal.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_620/MTY3ODI3NjE1MDA3MTIyNzA1/ccd05826.webp")
+
+@supported_shoulderstand = Pose.create!(name: "Supported Shoulderstand", description: "Laying on your back, bend your knees. Press your arms against the floor and push your feet away bringing yoru thighs in front of your torso. Continue to bring your legs forward and find balance as you being to straighten supporting your back with your hands.", difficulty: "Beginner", image: "https://www.yogajournal.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_620/MTQ3MTc4MTM4NjM0Mjk5MTgz/2anatomy_284_05_fnl.webp")
+
+@plow_pose = Pose.create!(name: "Plow Pose", description: "Starting from supported shoulderstand bend at hips and slowly lower your toes above your head. Rlease your hands from your back and clasp opposite of your feet.", difficulty: "Intermediate", image: "https://www.yogajournal.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_450/MTQ2MTgwNzcyNjA1MzM5MTY4/plow-pose-halasana.webp")
 puts "#{Pose.count} poses created"
 
 @example_log.poses.push(@downward_dog)
