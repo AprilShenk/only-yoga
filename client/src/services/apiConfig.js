e75032601542e2b3560d921a126a611be4803579
April Shenk <aprilshenk@MacBook-Pro.local>
2020-10-22 15:26:54 -0400

    production link added

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,20 +1,11 @@
 import axios from 'axios'
 
-let apiUrl
-
-const apiUrls = {
-    production: '',
-    development: 'http://localhost:3000'
-}
-
-if (window.location.hostname === 'localhost') {
-    apiUrl = apiUrls.development
-} else {
-    apiUrl = apiUrls.production
-}
+const baseUrl = process.env.NODE_ENV === 'production' ?
+ 'https://yoga-only-api.herokuapp.com/' :
+  'http://localhost:3000' 
 
 const api = axios.create({
-    baseURL: apiUrl
+  baseURL: baseUrl
 })
 
 export default api
