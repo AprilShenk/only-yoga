ef25b0f43bea44c842d8d8421aaf853b2c958c93
April Shenk <aprilshenk@MacBook-Pro.local>
2020-10-25 17:51:45 -0400

    Added tag like divs to log detail

--- client/src/screens/LogDetail.jsx
+++ client/src/screens/LogDetail.jsx
@@ -1,25 +1,77 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
+import styled from 'styled-components';
 
-const LogDetail = ({ logs }) => {
+const StyledDiv = styled.div`
+  background: #F0F7F4;
+  margin: 0 auto;
+  padding: 10px;
+  width: 70%;
+  border-radius: 20px;
+  color: #705D56;
+  font-weight: bold;
+  margin: 10px auto;
+`;
+const StyledPose = styled.div`
+  margin: 3px;
+  background: #70ABAF;
+  display: inline-block;
+  padding: 7px;
+  border-radius: 10px;
+  color: #32292F;
+  font-size: small;
+`;
+
+const ButtonDiv = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  width: 100%;
+  margin-top: 10px;
+
+  button {
+    background: #70ABAF;
+    width: 25%;
+    padding: 5px;
+    border-radius: 5px;
+    border: none;
+  }
+  button:hover {
+    background: #32292F;
+    color: #F0F7F4;
+  }
+`;
+
+const LogDetail = ({ logs, handleLogDelete }) => {
   const { id } = useParams();
+  const history = useHistory();
   const log = logs && logs.find(log => log.id === Number(id))
+  const routeChange = (e, id) => {
+    e.preventDefault()
+    history.push(`/logs/${id}/edit`)
+  }
 
   return (
-    <div>
+    <StyledDiv>
       {log &&
         <>
-          <h6>{new Date(log.created_at).toLocaleString()}</h6>
-        <p key={log.name}>Notes: {log.description}</p>
-        <div>
-          Poses: {log.poses ? log.poses.map(pose => (
-            <p key={pose.id}>{pose.name}</p>
-          )) :
-          ''}
+          <p>{new Date(log.created_at).toLocaleString()}</p>
+          <p key={log.name}>Notes: {log.description}</p>
+          <div>
+            <p>
+              Poses:
+            </p>
+            {log.poses ? log.poses.map(pose => (
+              <StyledPose key={pose.id}>{pose.name}</StyledPose>
+            )) :
+            ''}
         </div>
+        <ButtonDiv>
+            <button onClick={(e) => routeChange(e, log.id)}>Edit</button>
+            <button onClick={() => handleLogDelete(log.id)}>Delete</button>
+          </ButtonDiv>
         </>
       }
-    </div>
+    </StyledDiv>
   );
 };
 
